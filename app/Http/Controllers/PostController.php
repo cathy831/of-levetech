1077f90157c1cd7c380b84033ecda1604d4d9088
cathy831 <cathy_831@icloud.com>
2021-12-30 22:28:16 +0900

    データの表示応用

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)
-{
-    return view('index')->with(['posts' =>$post->get()]);
-}
+
+
+   {
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+   }
+
+
 }
